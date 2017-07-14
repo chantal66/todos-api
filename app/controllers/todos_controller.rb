6644dfe91ad8f8e1caaf5b99e1af196e08307617
Chantal Justamond <cjustamm66@me.com>
2017-07-14 13:08:29 -0600

    all test passing for todos request specs

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show]
+  before_action :set_todo, only: [:show, :update, :destroy]
 
   def index
     @todos = Todo.all
@@ -8,17 +8,27 @@ class TodosController < ApplicationController
 
   def create
     @todo = Todo.create!(todo_params)
-    json_response(@todo)
+    json_response(@todo, :created)
   end
 
   def show
     json_response(@todo)
   end
 
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
   private
 
   def todo_params
-    params.permit(:title, :create_by)
+    params.permit(:title, :created_by)
   end
 
   def set_todo
