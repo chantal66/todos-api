848d96c0ebe29d592dbf4f98048422b8d989d04f
Chantal Justamond <cjustamm66@me.com>
2017-07-21 11:01:16 -0600

    tests for application controller added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  # POST /signup
+  # return authenticated token upon signup
+
+  def create
+    user = User.create!(user_params)
+    auth_token = AthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+end
